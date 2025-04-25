54cb8236d73273280bbdf6047517938fd114d0d3
Ankit Kumar <ankit_kumar4@epam.com>
2025-04-25 18:33:49 +0530

    random commit2

--- frontend/src/components/organisms/Cars.tsx
+++ frontend/src/components/organisms/Cars.tsx
@@ -3,20 +3,20 @@ import Layout from "./Layout";
 import AlertBox from "../../context/alert/AlertBox";
 
 const Cars = () => {
-  const successAlert = {
-    type: "success",
-    heading: "Success!",
-    message: "Your operation was completed successfully.",
-  };
-  const errorAlert = {
-    type: "error", // Enum ensures type safety
-    heading: "Error!",
-    message: "Something went wrong during the operation.",
-  };
+  // const successAlert = {
+  //   type: "success",
+  //   heading: "Success!",
+  //   message: "Your operation was completed successfully.",
+  // };
+  // const errorAlert = {
+  //   type: "error", // Enum ensures type safety
+  //   heading: "Error!",
+  //   message: "Something went wrong during the operation.",
+  // };
   return (
     <Layout>
-      <AlertBox alert={successAlert} />
-      <AlertBox alert={errorAlert} />
+      {/* <AlertBox alert={successAlert} />
+      <AlertBox alert={errorAlert} /> */}
       Carspage
     </Layout>
   );
